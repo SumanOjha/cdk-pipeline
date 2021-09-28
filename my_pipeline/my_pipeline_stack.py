4c1fd9c75c931897b62f2279f97ce4fcdadbfa31
Suman Ojha <SumanOjha@snoop.net.au>
2021-09-28 11:07:32 +1000

    Remove the venv activation command

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -47,8 +47,7 @@ class MyPipelineStack(cdk.Stack):
                         synth=ShellStep("Synth", 
                             input=source,
                             # commands=["npm ci", "npm run build", "npx cdk synth"]
-                            commands=[  "source .venv/bin/activate",
-                                        "python -m pip install -r requirements.txt",
+                            commands=[  "python -m pip install -r requirements.txt",
                                         "npm config set unsafe-perm true",
                                         "npx cdk synth"
                             ]
